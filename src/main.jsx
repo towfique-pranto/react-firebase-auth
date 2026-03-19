9a8837bd2bfe9405595296b964edad245b01f17e
Towfique Kabir <towfique.pranto@gmail.com>
2026-03-20 03:42:49 +0600

    feat: add initial project setup with React, Vite, and routing
    - Added SVG assets for React and Vite.
    - Created CSS module for registration form styling.
    - Implemented global styles in index.css.
    - Set up main entry point with React Router for navigation.
    - Configured Vite for React application.

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router";
+import Login from "./Login.jsx";
+import Register from "./Register.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
