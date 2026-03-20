46f641aa61e5102d759757c2c848f295e3282f3a
Towfique Kabir <towfique.pranto@gmail.com>
2026-03-21 03:22:37 +0600

    feat: implement registration form with user input handling and submission

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,19 +1,54 @@
+import { useState } from "react";
 import { Link } from "react-router";
 import styles from "./css/register.module.css";
+import { registerUser } from "./firebase/firebase.config";
 
 function Register() {
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (e) => {
+    setUserInfo({ ...userInfo, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await registerUser(userInfo.email, userInfo.password);
+    console.log(userInfo);
+  };
+
   return (
     <div className={styles.container}>
-      <form action="" method="post" className={styles.form}>
+      <form
+        onSubmit={handleSubmit}
+        action=""
+        method="post"
+        className={styles.form}
+      >
         <h1 className={styles.heading}>Create Account</h1>
         <label htmlFor="name" className={styles.label}>
           Username
         </label>
-        <input type="text" name="name" id="name" className={styles.input} />
+        <input
+          type="text"
+          name="name"
+          id="name"
+          className={styles.input}
+          onChange={handleInputChange}
+        />
         <label htmlFor="email" className={styles.label}>
           Email
         </label>
-        <input type="email" name="email" id="email" className={styles.input} />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          className={styles.input}
+          onChange={handleInputChange}
+        />
         <label htmlFor="password" className={styles.label}>
           Password
         </label>
@@ -22,6 +57,7 @@ function Register() {
           name="password"
           id="password"
           className={styles.input}
+          onChange={handleInputChange}
         />
         <button type="submit" className={styles.button}>
           Register
